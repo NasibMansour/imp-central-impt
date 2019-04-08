996c8d24b6fb7e39d23afb1c6cdec0cc2cd6fb93
Nasib <44847138+NasibMansour@users.noreply.github.com>
2019-04-08 10:20:20 -0400

    Add --ac option to impt loginkey commands

--- bin/cmds/loginkey/update.js
+++ bin/cmds/loginkey/update.js
@@ -39,6 +39,7 @@ exports.describe = COMMAND_SHORT_DESCR;
 exports.builder = function (yargs) {
     const options = Options.getOptions({
         [Options.LOGIN_KEY] : true,
+        [Options.ACCOUNT] : false,
         [Options.PASSWORD] : false,
         [Options.DESCRIPTION] : {
             demandOption : true,
