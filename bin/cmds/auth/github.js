9943c3b0b31eca44887cdaa1b3dacb2a91ae9842
Nasib <44847138+NasibMansour@users.noreply.github.com>
2019-05-02 08:30:56 -0400

    Add auth github.js file

---
+++ bin/cmds/auth/github.js
@@ -0,0 +1,72 @@
+// MIT License
+//
+// Copyright 2018 Electric Imp
+//
+// SPDX-License-Identifier: MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+const Auth = require('../../../lib/Auth');
+const Options = require('../../../lib/util/Options');
+const Errors = require('../../../lib/util/Errors');
+const UserInteractor = require('../../../lib/util/UserInteractor');
+
+const COMMAND = 'github';
+const COMMAND_SECTION = 'auth';
+const COMMAND_SHORT_DESCR = 'Adds GitHub credentials to auth file.';
+const COMMAND_DESCRIPTION = COMMAND_SHORT_DESCR;
+
+exports.command = COMMAND;
+
+exports.describe = COMMAND_SHORT_DESCR;
+
+exports.builder = function (yargs) {
+    const options = Options.getOptions({
+        [Options.LOCAL] : false,
+        [Options.USER] : {
+            demandOption : false,
+            describe : 'A GitHub account username.',
+            _usage: '<github_username>'
+        },
+        [Options.PASSWORD] : {
+            demandOption : false,
+            describe : 'A GitHub account password or personal access token.',
+            _usage: '<github_password>'
+        },
+        [Options.OUTPUT] : false
+    });
+    return yargs
+        .usage(Options.getUsage(COMMAND_SECTION, COMMAND, COMMAND_DESCRIPTION, Options.getCommandOptions(options)))
+        .options(options)
+        .check(function (argv) {
+            const opts = new Options(argv);
+            if (opts.user === undefined && opts.password) {
+                return new Errors.CommandSyntaxError(UserInteractor.ERRORS.CMD_COOPERATIVE_OPTIONS, Options.PASSWORD, Options.USER);
+            }
+            return Options.checkOptions(argv, options);
+        })
+        .strict();
+};
+
+exports.handler = function (argv) {
+    const options = new Options(argv);
+    new Auth(options).github(options);
+};
